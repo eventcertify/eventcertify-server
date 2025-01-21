e17f8ea75d89c9c0971f6fbb98f8dd104acb30ca
Deepak Kumar <33062903+deepak-raaaz@users.noreply.github.com>
2025-01-21 23:48:44 +0530

    Delete dist directory

--- dist/app.local.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// YOUR_BASE/app.local.js
-const app = require('./app');
-app.listen(8080, () => console.log('Successfully connected to server!'));
-//# sourceMappingURL=app.local.js.map
